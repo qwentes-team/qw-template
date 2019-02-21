e8edd29a8c589b185858d99a596095cb69e43639
Matteo Barone <barmat90@gmail.com>
2019-02-21 16:45:50 +0100

    feat(accordion): implemented accordion WIP | #1

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
+import '@babel/polyfill';
 import Util from './util.js';
 import Carousel from './carousel.js';
 
 document.addEventListener('DOMContentLoaded', () => {
   initMenu();
   initCarousel();
+  initAccordions();
 });
 
 function initMenu() {
@@ -32,4 +34,34 @@ function initCarousel() {
 
   const testCarousel2 = '.test-carousel-no-progressbar';
   new Carousel(testCarousel2, { ...options, autoPlay: true });
+}
+
+function initAccordions() {
+  const accordions = Util.elements('.accordion');
+  const IS_OPEN_CLASS = 'is-open';
+
+  const closeAllAccordions = indexToNotClose => {
+    for (let i = 0; i < accordions.length; i++) {
+      indexToNotClose !== i && accordions[i].classList.remove(IS_OPEN_CLASS);
+    }
+  };
+
+  const toggleAccordion = (el, indexToNotClose) => {
+    const INTERNAL_TYPE = 'internal';
+    if (el.dataset.type !== INTERNAL_TYPE) {
+      closeAllAccordions(indexToNotClose);
+    }
+
+    el.classList.toggle(IS_OPEN_CLASS);
+  };
+
+  for (let i = 0; i < accordions.length; i++) {
+    const children = [...accordions[i].children];
+    const headerChildren = [...children[0].children];
+    const arrow = headerChildren.find(child => child.className === 'accordion__arrow');
+    const minusPlus = headerChildren.find(child => child.className === 'accordion__minus-plus');
+
+    arrow && arrow.addEventListener('click', () => toggleAccordion(accordions[i], i));
+    minusPlus && minusPlus.addEventListener('click', () => toggleAccordion(accordions[i], i));
+  }
 }
